b2a646bf166f29fb3d6a1be3a099fa0e05477a6d
tomason369 <78355814+tomason369@users.noreply.github.com>
2021-11-19 23:43:47 +0100

    update discordjs

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
+const bot = new Discord.Client({
+    intents: (
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    )
+});
+const config = require("./config.json")
 
 bot.on("ready", function (){
     console.log(`${bot.user.username} is online`);
 });
 
-bot.login(process.env.token)
+bot.login(config.token);
