3cf5273da9504f337b529f6ac82e7db933b348a3
tomason369 <78355814+tomason369@users.noreply.github.com>
2021-11-19 23:19:09 +0100

    první

---
+++ index.js
@@ -0,0 +1,22 @@
+const discord = require("discord.js")
+const client = new discord.Client({
+    intents: (
+        discord.Intents.FLAGS.GUILDS,
+        discord.Intents.FLAGS.GUILD_MESSAGES
+    )
+});
+const config = require(`./config.json`)
+
+client.on("ready", () => {
+    console.log("Jsem ready");
+});
+
+client.on("messageCreate", (message) => {
+    if (message.content === "ping") {
+        message.reply({
+            content: "pong"
+        })
+    }
+})
+
+client.login(config.token)
