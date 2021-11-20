4bf95bd9991b36585111fceb876fb1ee873b2610
tomason369 <78355814+tomason369@users.noreply.github.com>
2021-11-20 18:23:51 +0100

    celková aktualizace

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({
-    intents: (
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES
-    )
-});
-const config = require("./config.json")
+const client = new Discord.Client({
+    partials: ["CHANNEL", "MESSAGE"],
+    restTimeOffset: 0,
+    intents: 513,
 
-bot.on("ready", function (){
-    console.log(`${bot.user.username} is online`);
+});
+const config = require("./config.json");
+const fs = require("fs")
+
+client.on("ready", () => {
+    console.log("Jsem ready");
+    let statuses = config.statuses
+
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random()* statuses.length)];
+        client.user.setActivity(status,{type: "PLAYING"});
+    },10000)
 });
 
-bot.login(config.token);
+client.on("messageCreate", (message) => {
+    if (!message.guild) return;
+    if (message.author.bot) return;
+    if(!message.content.startsWith(config.prefix)) return;
+    if(message.content.startsWith(config.prefix+"ping")){
+        message.channel.send({
+            content: `Pong \nPing: ${client.ws.ping}ms`
+        })
+    }
+})
+
+
+client.login(config.token)
+
